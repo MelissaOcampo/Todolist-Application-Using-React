6e48569e76bad50b67158c88ea168d1fccbde482
MelissaOcampo <melissa.ocampo.2001@gmail.com>
2023-02-03 00:43:21 +0000

    nuevos cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,135 +1,117 @@
-import React, {useState,useEffect} from "react";//importo react y el hook useState
-//create your first component
-const Home = () => {
+import React, {useState, useEffect} from "react";
+// importo react y el hook useState
+// create your first component
+const Home = () => { // declaro los estados//
+    const [pendientes, setPendientes] = useState("")
+    // 1. creamos un estado del input pendientes
+    // [espacio donde guardo los valores,funcion que actualiza los valores]
+    // creo funcion para ejecutar la actualizacion del valor inicial
+    const [info, setInfo] = useState([]); // 2. creamos un estado de lo que se guarda en el array
 
-	//declaro los estados//
-	const[pendientes,setPendientes]=useState("")//1. creamos un estado del input pendientes
-        //[espacio donde guardo los valores,funcion que actualiza los valores]
-	//creo funcion para ejecutar la actualizacion del valor inicial
-	const [info, setInfo] = useState([]); // 2. creamos un estado de lo que se guarda en el array
-	function handlePendientes (e) {
-		//console.log(e.target.value)//para poder ver el valor del target en el que esta posicionado
-		setPendientes(e.target.value) 
-	}
-	function enviarPendientes (e){
-		e.preventDefault()// detenemos el comportamiento predeterminado para procesar nuestro codigo
-		setInfo(info.concat({label: pendientes, done: false}))
-		setPendientes("")
-	} 
-	// eliminar item en array
-	const deleteItem = (indexItem) => {
-		setInfo(info.filter((infoItem, index) => {
-		  return index != indexItem;
-		}))
-	  }
+    const deletePendientes = (indexItem) => {
+        setInfo((prevState) => prevState.filter((listItems, index) => index !== indexItem));
+    };
+    function agregarPendientes(e) {
+        e.preventDefault()
+        setInfo(info.concat({label: pendientes, done: false}))
+        setPendientes("")
+    }
 
 
-	//   const deleteItem = (indexItem) => {
-	// 	setInfo((prevState) =>
-	// 	  prevState.filter((Items, index) => index !== indexItem)
-	// 	);
-	//   };
+    function crearUsuario() {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/gmm`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify([])
 
-	  //el array
-// 	  const listItems = info.map((item, index) =>
-//   <li>{item} <button onClick={() => remove(index)}>
-//   </button>X</li>
-//   );
-	function crearUsuario() {
-		fetch(`https://assets.breatheco.de/apis/fake/todos/user/gmm`,{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			  },
-			  body: JSON.stringify([]) 
-			  
-		})
-		.then((response) => response.json())
- 		.then((data) => console.log(data));
-	}
-	console.log(crearUsuario())
+        }).then((response) => response.json()).then((data) => console.log(data));
+    }
+    console.log(crearUsuario())
 
-	function obtenerData() {
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/gmm',{
-			method: 'GET',
-		})
-		.then((response) => response.json())
- 		.then((data) => setInfo (data));
-	}
+    function obtenerInfo() {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/gmm', {method: 'GET'}).then((response) => response.json()).then((data) => setInfo(data));
+    }
 
 
-	function actualizarInfo() {
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/gmm',{
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json'
-			  },
-			  body: JSON.stringify(info) 
-			  
-		})
-		.then((response) => response.json())
- 		.then((data) => console.log(data));
-	}
-	console.log(actualizarInfo())
+    function actualizarInfo() {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/gmm', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(info)
 
-	function eliminarInfo() {
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/gmm',{
-			method: 'DELETE',
-			headers: {
-				'Content-Type': 'application/json'
-			  },
-		})
-		.then((response) => response.json())
- 		.then((data) => {console.log(data.result)
-			if (data.result==="okey"){
-				setInfo ([])
-				}}
-			); 
-	}
+        }).then((response) => response.json()).then((data) => console.log(data));
+    }
+    console.log(actualizarInfo())
 
-	useEffect(()=>{
-		crearUsuario();
-		obtenerData()
-	},[])
+    function eliminarInfo() {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/gmm', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => response.json()).then((data) => {
+            console.log(data.result)
+            if (data.result === "ok") {
+                setInfo([])
+            }
+        });
+    }
+
+    useEffect(() => {
+        crearUsuario();
+        obtenerInfo()
+    }, [])
 
 
-
-	useEffect (()=>{
-		actualizarInfo()
-		},[info])
-		console.log(info)
-
+    useEffect(() => {
+        actualizarInfo()
+    }, [info])
+    console.log(info)
 
 
-
-
-	return (
-		<>
-		<form className="container" onClick={enviarPendientes}>
-  <div className="mb-2">
-    <label htmlFor="exampleInputEmail1" className="form-label">todos</label>
-	 {/*2. definimos el evento ochange en el input */}
-	 <input type="text" className="form-control" onChange={handlePendientes} value={pendientes}/>
-  </div>
-  <button type="add" onClick={enviarPendientes} className="btn btn-dark">add</button>
-  <div id="contenedorData" className="text-light">
-  {
+    return (
+        <>
+            <div className="card container d-flex bg-success mt-5 md-w75 pb-5">
+                <h2 className="titulo m-auto p-2">LISTA DE PENDIENTES</h2>
+                <div className="card-body">
+                    <input type="text" className="input m-1 w-75"
+                        value={pendientes}
+                        id="exampleInput"
+                        aria-describedby="inputHelp"
+                        onChange={
+                            (e) => {
+                                setPendientes(e.target.value)
+                            }
+                        }
+                        placeholder="Añadir pendiente"/>
+                    <button type="submit" className="btn btn-secondary btn-sm"
+                        onClick={agregarPendientes}>Añadir</button> 
+						<button type="submit" className="btn btn-danger btn-sm"
+                        onClick={eliminarInfo}>Eliminar pendientes</button>
+                </div>
+                <div className="to-do-list d-flex bg-warning ">
+                    <ul>{
                         info.map((item, index) => (
-                            <div  className="row d-flex m-2"  style={{borderRadius:"20px",backgroundColor: "rgb(255, 99, 71)"}} key= {index}> 
-
-                                <div className="col-6">
-                                    <h5 className="m-2">{item.label} </h5>
-                                </div>
-								<div className="col-6 text-end" onClick={() => deleteItem(indexItem)}>
-                                    <i className="fas fa-trash-alt align-items-end m-2 pt-1"></i>
-                                </div>
-                            </div>
+                            <li key={index}>
+                                {
+                                item.label
+                            }
+                                <button className="btn"
+                                    onClick={
+                                        () => deletePendientes(index)
+                                }>
+                                    <i className="fas fa-trash-alt"/>
+                                </button>
+                            </li>
                         ))
-                    } </div>
-		</form>
-<div>
-</div>
-		</>
-	);
+                    }</ul>
+                </div>
+            </div>
+        </>
+    );
 };
 export default Home;
